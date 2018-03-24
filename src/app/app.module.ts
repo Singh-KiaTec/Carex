07f67bc2797deb0678f78a43937befba1b41da39
KiaTec <vsi@kiatec.dk>
2018-03-24 18:05:22 +0100

    basic swipe and buttons integrattion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,52 @@
+//DEFAULT
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
+
+//ANGULAR
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home.page';
+import { ListPage } from '../pages/list/list';
+import {MenuPage} from '../pages/menu/menu.page';
+
+
+//COMPONENTS
+import{MenuComponent} from '../components/menu/menu';
+
+
+
+//MODELS
+import { data } from '../models/data/data'; 
+import { homepagedata } from '../models/data/homepagedata'; 
+
+
+//PROVIDERS
+import {BaseRestService} from '../providers/restservice/base.rest.service';
+
+
+
+//IONIC
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+const pagesDeclaration =[
+MenuPage,
+MyApp,
+HomePage,
+ListPage
+];
+
+const componentDeclaration = [
+  MenuComponent
+
+];
+
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
-    ListPage
+    ...pagesDeclaration,
+    ...componentDeclaration
   ],
   imports: [
     BrowserModule,
@@ -23,11 +56,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    MenuPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    BaseRestService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
