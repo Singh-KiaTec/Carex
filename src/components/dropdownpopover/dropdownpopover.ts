a66342741250b863341fa043366835bc6b826003
KiaTec <vsi@kiatec.dk>
2018-07-12 12:10:14 +0200

    Version 0.0.0

---
+++ src/components/dropdownpopover/dropdownpopover.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'dropdownpopover.html'
+})
+export class DropDownPopOver {
+
+    constructor() {
+
+    }
+
+
+    dismiss() {
+    }
+}
