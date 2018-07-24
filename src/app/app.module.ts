451d894f8c2912990b7b5b65b0b83f14c331db32
KiaTec <vsi@kiatec.dk>
2018-07-24 10:36:39 +0200

    new UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -77,31 +77,31 @@ import { PopoverIonicdeploy } from '../components/ionicpopover/ionicpopover';
 import { DropDownPopOver } from '../components/dropdownpopover/dropdownpopover';
 
 
-Pro.init('2564d9e8', {
-  appVersion: '0.0.9',
-  channel: 'master'
-});
+// Pro.init('2564d9e8', {
+//   appVersion: '0.0.11',
+//   channel: 'master'
+// });
 
-@Injectable()
-export class MyErrorHandler implements ErrorHandler {
-  ionicErrorHandler: IonicErrorHandler;
+// @Injectable()
+// export class MyErrorHandler implements ErrorHandler {
+//   ionicErrorHandler: IonicErrorHandler;
 
-  constructor(injector: Injector) {
-    try {
-      this.ionicErrorHandler = injector.get(IonicErrorHandler);
-    } catch (e) {
-      // Unable to get the IonicErrorHandler provider, ensure
-      // IonicErrorHandler has been added to the providers list below
-    }
-  }
+//   constructor(injector: Injector) {
+//     try {
+//       this.ionicErrorHandler = injector.get(IonicErrorHandler);
+//     } catch (e) {
+//       // Unable to get the IonicErrorHandler provider, ensure
+//       // IonicErrorHandler has been added to the providers list below
+//     }
+//   }
 
-  handleError(err: any): void {
-    Pro.monitoring.handleNewError(err);
-    // Remove this if you want to disable Ionic's auto exception handling
-    // in development mode.
-    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
-  }
-}
+//   handleError(err: any): void {
+//     Pro.monitoring.handleNewError(err);
+//     // Remove this if you want to disable Ionic's auto exception handling
+//     // in development mode.
+//     this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+//   }
+// }
 
 const pagesDeclaration = [
   MenuPage,
@@ -207,8 +207,8 @@ const componentDeclaration = [
     WindowRef,
     AuthService,
     InAppBrowser,
-    // { provide: ErrorHandler, useClass: IonicErrorHandler }
-    [{ provide: ErrorHandler, useClass: MyErrorHandler }]
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    //[{ provide: ErrorHandler, useClass: MyErrorHandler }]
   ]
 })
 export class AppModule { }
