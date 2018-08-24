611b3a6fcf47279ce3b53767377e0f4484c7523f
KiaTec <vsi@kiatec.dk>
2018-08-24 11:25:22 +0200

    profile page name email etc fix

--- src/components/profile/profile.ts
+++ src/components/profile/profile.ts
@@ -1,59 +1,68 @@
 import { Component } from '@angular/core';
 import { NavParams, Navbar } from 'ionic-angular';
 import { ProfilePage } from '../../pages/profile/profile.page';
-import {BaseRestService} from '../../providers/restservice/base.rest.service';
-import {WelcomePage} from '../../pages/welcome/welcome.page'
+import { BaseRestService } from '../../providers/restservice/base.rest.service';
+import { WelcomePage } from '../../pages/welcome/welcome.page'
 import { NavController } from 'ionic-angular';
-import {StorageService} from '../../providers/storageservice/storageservice'
-import {AuthService} from '../../providers/authenticationservice/auth.service';
+import { StorageService } from '../../providers/storageservice/storageservice'
+import { AuthService } from '../../providers/authenticationservice/auth.service';
 // import { User } from '../../models/user.model';
 // import {WindowRef} from '../../providers/windowservice/windowservice';
 // import {Cookie} from 'ng2-cookies';
 // import {CookieService} from 'ngx-cookie-service';
 import { LoginPage } from '../../pages/login/login.page';
 
-declare var window:any;
+declare var window: any;
 
 @Component({
-    selector:'profile-viewer',
+    selector: 'profile-viewer',
     templateUrl: 'profile.html'
 })
 export class ProfileComponent {
     rootPage: any = ProfilePage;
     private loggedout;
     private loggedUrl;
-    private user:any;
-    private profileContent:any;
-    private loading:boolean;
-private username;
-private name;
-private email;
-    
+    private user: any;
+    private profileContent: any;
+    private loading: boolean;
+    private username;
+    private name;
+    private email;
 
-    constructor(private baserestService:BaseRestService,
-        private navCtrl: NavController, 
+
+    constructor(private baserestService: BaseRestService,
+        private navCtrl: NavController,
         private navParam: NavParams,
-        private auth: AuthService,private storageService: StorageService) { }
+        private auth: AuthService, private storageService: StorageService) { }
 
     ngOnInit() {
         // Tracking
         this.loggedUrl = this.auth.getEnvironment();
-      //  this.user = this.auth.getUserInfo();
-      this.user = this.navParam.get('userinfo');
+        //  this.user = this.auth.getUserInfo();
+        this.user = this.navParam.get('userinfo');
+
+        if (this.user.brugernavn) {
+            this.username = this.user.brugernavn;
+            this.email = this.user.email;
+            this.name = this.user.username;
+        }
+        else {
+            this.username = this.user[2];
+            this.email = this.user[1];
+            this.name = this.user[0];
+        }
 
 
-     this.username = this.user.brugernavn;
-     this.email = this.user.email;
-     this.name = this.user.username;
+
 
         console.log(this.user);
         this.baserestService.getProfileContent().then(
             profileContent => { this.profileContent = profileContent; this.setData(); this.loading = false },
             error => { this.loading = false }
-        );        
+        );
 
 
-  
+
     }
 
     setData() {
@@ -61,19 +70,20 @@ private email;
     }
 
 
-    
-    logout(){
-       // this.storageService.clear();
-       //this.cookieService.delete('http://udv-admin.carex.dk');
+
+    logout() {
+        // this.storageService.clear();
+        //this.cookieService.delete('http://udv-admin.carex.dk');
         this.baserestService.logout(this.loggedUrl).then(
-           loggedout => {
-               this.loggedout = loggedout;
-            this.initializeApp()},
-           error=>{console.error(error); this.initializeApp()}
-            
+            loggedout => {
+                this.loggedout = loggedout;
+                this.initializeApp()
+            },
+            error => { console.error(error); this.initializeApp() }
+
         )
     }
-    initializeApp(){
+    initializeApp() {
         this.storageService.clear();
         this.navCtrl.setRoot(WelcomePage);
     }
